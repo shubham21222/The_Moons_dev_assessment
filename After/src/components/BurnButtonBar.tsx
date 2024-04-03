93b06e434b5b99fce807f8fff067d130511bb4a2
shubham raikwar <119775775+shubham21222@users.noreply.github.com>
2024-04-03 14:19:53 +0530

    add  comments in  BurnButtonBar.tsx

--- After/src/components/BurnButtonBar.tsx
+++ After/src/components/BurnButtonBar.tsx
@@ -1,4 +1,17 @@
+import React from 'react';
 
+/**
+ * Component for rendering the burn button bar.
+ * @param {Object} props - The props object.
+ * @param {string} props.burnAmount - The amount of tokens to burn.
+ * @param {Function} props.onChangeBurnAmount - Function to handle input change for burn amount.
+ * @param {Function} props.executeBurn - Function to execute the burn process.
+ * @param {string} props.txButton - The text to display on the burn button.
+ * @param {boolean} props.txProgress - Flag indicating whether a transaction is in progress.
+ * @param {string} props.burnTxHash - The hash of the burn transaction.
+ * @param {string} props.walletChain - The chain of the wallet.
+ * @returns {JSX.Element} - JSX element representing the burn button bar.
+ */
 const BurnButtonBar = ({
   burnAmount,
   onChangeBurnAmount,
@@ -10,13 +23,17 @@ const BurnButtonBar = ({
 }) => {
   return (
     <div className="info_box filled">
+      {/* Box Subheader */}
       <p className="box_subheader">Burn your App</p>
+      {/* Description */}
       <div className="description medium">
         &quot; The process of reducing the supply of App tokens by
         permanently removing a certain number of them from circulation,
         often through a deliberate and recorded mechanism. &quot;
       </div>
+      {/* Burn Bar */}
       <div className="burn_bar">
+        {/* Input Value Box */}
         <div className="input_value_box">
           <p className="input_muted">Enter amount to Burn</p>
           <input
