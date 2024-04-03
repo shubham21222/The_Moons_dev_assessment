a3702706fcedd66f840b5828017f730b658c5c59
shubham21222 <shubhamraikwar@hotmail.com>
2024-04-03 13:14:34 +0530

    added utils named address

---
+++ After/src/utils/addressUtils.ts
@@ -0,0 +1,7 @@
+//AddressUtils.ts
+
+export const fetchAddressForChain = (chainId, tokenType) => {
+    //logic  for fetching address
+    }
+    // Fetch the token address based on the chainId and tokenType
+return tokenAddresses[chainId][tokenType];
