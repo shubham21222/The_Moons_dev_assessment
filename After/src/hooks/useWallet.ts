a22be2c64e8742d22abfa401c38fc0985e1be8f5
shubham21222 <shubhamraikwar@hotmail.com>
2024-04-03 13:09:12 +0530

    added hook named usewallet

---
+++ After/src/hooks/useWallet.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+const useWallet = () => {
+  const [walletAddress, setWalletAddress] = useState<string | null>(null);
+  const [isWalletConnected, setIsWalletConnected] = useState<boolean>(false);
+  const [openChainModal,setopenChainModal] = useState<string | null>(null);
+  const [walletChain,setwalletChain] = useState<string | null>(null)
+  const [openConnectModal,setopenConnectModal] = useState<string | null>(null)
+  const [isBalanceError,setisBalanceError] = useState<string | null>(null)
+  // Other state variables and functions related to wallet management
+
+  // Use useEffect to initialize wallet connection 
+  useEffect(() => {
+    // logic for initializing wallet connection or fetching wallet data
+  }, []);
+
+  // Define functions or methods for interacting with the wallet
+
+  return {
+    walletAddress,
+    isWalletConnected,
+    openChainModal,
+    walletChain,
+    isBalanceError,
+    openConnectModal
+    // Other state variables and functions you want to expose
+  };
+};
+
+export default useWallet;
