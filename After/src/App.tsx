ff309665ede71ecfd0fd7ed4377bbb874ae87925
shubham21222 <shubhamraikwar@hotmail.com>
2024-04-03 12:56:39 +0530

    added App typescript file

---
+++ After/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import BurnPage from './pages/Burnpage'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <BurnPage/>
+      </div>
+    </>
+  )
+}
+
+export default App
