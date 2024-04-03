671c5239b79ca16abcf29a9488f186a269e0cd5f
shubham raikwar <119775775+shubham21222@users.noreply.github.com>
2024-04-03 14:29:10 +0530

    add some lines of comments in  useAppToast.ts

--- After/src/hooks/useAppToast.ts
+++ After/src/hooks/useAppToast.ts
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+// Enum defining different severity levels for toasts
 export enum ToastSeverity {
   SUCCESS = 'success',
   ERROR = 'error',
@@ -7,19 +8,31 @@ export enum ToastSeverity {
   INFO = 'info',
 }
 
+/**
+ * Custom React hook for managing toast messages and their severity levels.
+ * @returns {Object} - Object containing toast message, toast severity, and a function to show toasts.
+ */
 const useAppToast = () => {
+  // State variables to manage toast message and severity
   const [toastMsg, setToastMsg] = useState<string | null>(null);
   const [toastSev, setToastSev] = useState<ToastSeverity | null>(null);
 
+  /**
+   * Function to show toast message with optional severity level.
+   * @param {string} message - The message to be displayed in the toast.
+   * @param {ToastSeverity} severity - The severity level of the toast (default: INFO).
+   */
   const showToast = (message: string, severity: ToastSeverity = ToastSeverity.INFO) => {
+    // Set toast message and severity
     setToastMsg(message);
     setToastSev(severity);
     setTimeout(() => {
       setToastMsg(null);
       setToastSev(null);
-    }, 5000); // Hide toast after 5 seconds
+    }, 5000);
   };
 
+  // Return state variables and the function to show toasts
   return {
     toastMsg,
     toastSev,
