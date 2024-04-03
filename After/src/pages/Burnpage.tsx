6996d7b4d3226f655b5984f862d18ce8b1bc92a0
shubham raikwar <119775775+shubham21222@users.noreply.github.com>
2024-04-03 14:25:07 +0530

    added page named Burnpage.tsx

--- After/src/pages/Burnpage.tsx
+++ After/src/pages/Burnpage.tsx
@@ -2,13 +2,17 @@
 
 import React from 'react';
 import useBurnPage from '../hooks/useBurnPage';
-import BurnTxProgress from '../constants/BurnTxProgress';
-import { numberWithCommas } from '../utils/numberUtils';
 import BurnButtonBar from '../components/BurnButtonBar';
 import BurnStatsContainer from '../components/BurnStatsContainer';
 import TransactionTable from '../components/TransactionTable';
 import ChainSelector from '../components/ChainSelector';
+import AppToast from '../hooks/useAppToast';
+import DashboardLayoutStyled from '../components/DashboardLayout';
 
+/**
+ * Component for rendering the burn page.
+ * @returns {JSX.Element} - JSX element representing the burn page.
+ */
 const BurnPage = () => {
   const {
     burnTransactions,
@@ -22,9 +26,45 @@ const BurnPage = () => {
 
   return (
     <div>
-      {/* Your JSX code using components and hooks */}
+      <DashboardLayoutStyled className="burnpage">
+        <div className="top_conatiner burnpage" style={{ alignItems: 'flex-start' }}>
+          <div className="info_box filled">
+            <h1 className="title">App TOKEN BURN</h1>
+            <p className="description medium"></p>
+
+            {/* Burn Button Bar */}
+            <BurnButtonBar
+              burnAmount={burnAmount}
+              onChangeBurnAmount={onChangeBurnAmount}
+              executeBurn={executeBurn}
+              txButton={txButton}
+              txProgress={txProgress}
+              burnTxHash={burnTxHash}
+            />
+          </div>
+          {/* Burn Stats Container */}
+          <BurnStatsContainer
+            burnTransactions={burnTransactions}
+            burnTxHash={burnTxHash}
+          />
+        </div>
+      </DashboardLayoutStyled>
+
+      {/* Transaction Table */}
+      <TransactionTable data={burnTransactions} />
+
+      {/* Chain Selector */}
+      <ChainSelector 
+        title={"Switch Token Chain"}
+      />
+
+      {/* App Toast */}
+      <AppToast
+        position={{ vertical: 'bottom', horizontal: 'center' }}
+      />
     </div>
   );
 };
 
 export default BurnPage;
+
